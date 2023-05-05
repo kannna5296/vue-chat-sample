29afa2733a1516d7b9a2a0ab7a4ff0e8faf3cb54
kannna5296 <kannnahacks5296@gmail.com>
2023-05-05 12:18:32 +0900

    サブメニュー切り出し(+Vue3化)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
 <script setup lang="ts">
+import { ref, Ref } from "vue";
 import Chat from "@/components/Chat.vue";
+
+const drawer = ref();
+type Link = {
+  icon: string;
+  text: string;
+};
+const links: Ref<Link[]> = ref([
+  { icon: "mdi-send", text: "hoge" },
+  { icon: "mdi-send", text: "hoge2" },
+]);
 </script>
 
 <template>
   <Chat />
+  <v-app>
+    <v-navigation-drawer v-model="drawer">
+      <v-sheet color="grey-lighten-4" class="pa-4">
+        <v-avatar class="mb-4" color="grey-darken-1" size="64"></v-avatar>
+
+        <div>john@google.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.icon" link>
+          <template v-slot:prepend>
+            <v-icon>{{ link.icon }}</v-icon>
+          </template>
+
+          <v-list-item-title>{{ link.text }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
 </template>
 
 <style scoped>
