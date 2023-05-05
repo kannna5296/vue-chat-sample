21127f2e6007709aa7d5a443ee6a6dd53547c4fc
kannna5296 <kannnahacks5296@gmail.com>
2023-05-05 18:44:30 +0900

    チャット送信可能にする

--- src/components/pages/ChatBoard.vue
+++ src/components/pages/ChatBoard.vue
@@ -7,16 +7,14 @@ const cards = ref(["today"]);
 const route = useRoute();
 const userId = route.query.user_id;
 
-const clear = () => {
-  console.log("Clear!");
-};
-
-const submit = () => {
-  console.log("Submit!");
-  console.log("chatData: ", chatData.value);
-};
-
 const chatData = ref("");
+
+//Backend介さない場合の初期値
+const messages = ref([
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique",
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique",
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique",
+]);
 
 const isValidText = computed(() => {
   console.log("computed");
@@ -27,6 +25,15 @@ const isValidText = computed(() => {
   console.log("valid!");
   return true;
 });
+
+const clear = () => {
+  chatData.value = "";
+};
+
+const submit = () => {
+  messages.value.push(chatData.value);
+  chatData.value = "";
+};
 </script>
 
 <!-- 公式(https://vuetifyjs.com/en/wireframes/inbox/)をベースとする -->
@@ -39,23 +46,19 @@ const isValidText = computed(() => {
           <v-card>
             <v-list lines="two">
               <v-list-subheader>{{ card }}</v-list-subheader>
-              <template v-for="n in 6" :key="n">
+              <template v-for="(message, index) in messages" :key="message">
                 <v-list-item>
                   <template v-slot:prepend>
                     <v-avatar color="grey-darken-1"></v-avatar>
                   </template>
 
                   <v-list-item-subtitle class="message">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Nihil repellendus distinctio similique
+                    {{ message }}
                   </v-list-item-subtitle>
                 </v-list-item>
+                <p>{{ index }}</p>
 
-                <v-divider
-                  v-if="n !== 6"
-                  :key="`divider-${n}`"
-                  inset
-                ></v-divider>
+                <v-divider inset></v-divider>
               </template>
             </v-list>
           </v-card>
