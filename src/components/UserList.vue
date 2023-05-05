ce2d299db190ab6eaa6f4a2f0220772945294c1b
kannna5296 <kannnahacks5296@gmail.com>
2023-05-05 16:32:20 +0900

    router追加

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -13,7 +13,9 @@
     <v-container>
       <v-row>
         <v-col v-for="n in 24" :key="n" cols="4">
-          <v-avatar color="grey lighten-2" size="128"></v-avatar>
+          <router-link :to="{ path: '/about', query: { user_id: n } }">
+            <v-avatar color="grey lighten-2" size="128"></v-avatar>
+          </router-link>
         </v-col>
       </v-row>
     </v-container>
