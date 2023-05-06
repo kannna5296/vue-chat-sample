280310a8f62576a01e70c2e49b52d69c5a6f465e
kannna5296 <kannnahacks5296@gmail.com>
2023-05-06 11:32:57 +0900

    ボタン類調整

--- src/components/pages/Login.vue
+++ src/components/pages/Login.vue
@@ -2,17 +2,15 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const name = ref("");
+const email = ref("");
+const password = ref("");
 
 const requiredValidation = (value: string) => !!value || "Name is required";
 const limitLengthValidation = (value: string) =>
   (value && value.length <= 10) || "Name is required";
-const nameRules = [requiredValidation, limitLengthValidation];
+const emailRules = [requiredValidation, limitLengthValidation];
+const passwordRules = [requiredValidation, limitLengthValidation];
 
-const select = ref();
-
-const items = ref(["Item 1", "Item 2"]);
-const checkbox = ref(false);
 const valid = ref(true);
 
 const validate = async () => {
@@ -27,31 +25,6 @@ const reset = async () => {
 const resetValidation = async () => {
   resetValidation();
 };
-// export default {
-//   data: () => ({
-//     // valid: true,
-//     // // name: "",
-//     // // nameRules: [
-//     // //   (v) => !!v || "Name is required",
-//     // //   (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
-//     // // ],
-//     // select: null,
-//     // items: ["Item 1", "Item 2", "Item 3", "Item 4"],
-//     // checkbox: false,
-//   }),
-//   methods: {
-//     // async validate() {
-//     //   const { valid } = await this.$refs.form.validate();
-//     //   if (valid) alert("Form is valid");
-//     // },
-//     reset() {
-//       this.$refs.form.reset();
-//     },
-//     resetValidation() {
-//       this.$refs.form.resetValidation();
-//     },
-//   },
-// };
 </script>
 
 <template>
@@ -61,30 +34,24 @@ const resetValidation = async () => {
       <v-card-subtitle>ユーザ情報を入力ください</v-card-subtitle>
       <v-form ref="form">
         <v-text-field
-          v-model="name"
-          :counter="10"
-          :rules="nameRules"
-          label="Name"
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          hint="Enter your email to access this website"
           required
         ></v-text-field>
 
-        <v-select
-          v-model="select"
-          :items="items"
-          :rules="[(v) => !!v || 'Item is required']"
-          label="Item"
+        <v-text-field
+          v-model="password"
+          :rules="passwordRules"
+          label="Password"
+          type="password"
+          hint="Enter your password to access this website"
           required
-        ></v-select>
+        ></v-text-field>
 
-        <v-checkbox
-          v-model="checkbox"
-          :rules="[(v) => !!v || 'You must agree to continue!']"
-          label="Do you agree?"
-          required
-        ></v-checkbox>
-
-        <div class="d-flex flex-column">
-          <v-btn color="success" class="mt-4" block @click="validate">
+        <!-- <div class="d-flex flex-column"> -->
+        <!-- <v-btn color="success" class="mt-4" block @click="validate">
             Validate
           </v-btn>
 
@@ -94,8 +61,10 @@ const resetValidation = async () => {
 
           <v-btn color="warning" class="mt-4" block @click="resetValidation">
             Reset Validation
-          </v-btn>
-        </div>
+          </v-btn> -->
+        <v-btn color="success">LOGIN</v-btn>
+        <v-btn>CLEAR</v-btn>
+        <!-- </div> -->
       </v-form>
     </v-card>
   </div>
@@ -104,6 +73,8 @@ const resetValidation = async () => {
 <style scoped>
 .login-container {
   background-color: rgb(255, 178, 171);
+  width: 100%;
+  height: 100%;
 }
 .login-form {
   margin: 150px auto;
